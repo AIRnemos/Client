0c7e4dd00cd01cd96eb521b90274f4f077fc5ab8
Quentin Schuster <quentin-schuster@tutanota.com>
2024-02-03 16:46:00 +0100

    Add license

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+/*
+    AIRnemos is a software for CO2 meter.
+    Copyright (C) 2023 Quentin Schuster
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include "main.h"
 
 TaskHandle_t task_led;
@@ -43,16 +60,10 @@ void setup() {
     StateNormal::start();
 
   log_i("Started airnemos core.");
+  vTaskDelete(NULL);
 }
 
-void loop() {
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  if(digitalRead(8) == LOW) {
-    LittleFS.remove("/config.json");
-    LittleFS.remove("/VERSION");
-    ESP.restart();
-  }
-}
+void loop() {}
 
 void error(bool ledInit) {
   if(ledInit) {
--- web/src/setup/check.svelte
+++ web/src/setup/check.svelte
@@ -1,4 +1,21 @@
 <script>
+    /*
+    AIRnemos is a software for CO2 meter.
+    Copyright (C) 2023 Quentin Schuster
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
     import { canNext } from "./store";
     import { onMount } from "svelte";
     import { _ } from "svelte-i18n"
