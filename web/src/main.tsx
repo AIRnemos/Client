1c0991f55023cca208dcb1387c996cc3ad69bbb6
Quentin Schuster <quentin-schuster@tutanota.com>
2024-06-26 16:37:22 +0200

    add setup

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,6 +5,14 @@ import "./app.css"
 
 import { routeTree } from './routeTree.gen'
 import { MeasurementProvider } from './provider/measurement'
+import { Setup } from './setup'
+
+function stringToBool(value: string): boolean {
+  return value == "false" ? false : true;
+}
+
+AIRNEMOS_SETUP = import.meta.env.DEV ? stringToBool(import.meta.env.AIRNEMOS_SETUP) : stringToBool(AIRNEMOS_SETUP)
+AIRNEMOS_AP = import.meta.env.DEV ? stringToBool(import.meta.env.AIRNEMOS_AP) : stringToBool(AIRNEMOS_AP)
 
 const router = createRouter({ routeTree })
 
@@ -14,10 +22,16 @@ declare module '@tanstack/react-router' {
   }
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <MeasurementProvider>
+const inner = AIRNEMOS_SETUP ? 
+  <Setup /> :
+
+  <MeasurementProvider>
       <RouterProvider router={router} />
     </MeasurementProvider>
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {AIRNEMOS_SETUP}
+    { inner }
   </React.StrictMode>,
 )
---
+++ web/src/setup/index.tsx
@@ -0,0 +1,3 @@
+export function Setup() {
+    return <p>Setup</p>
+}
