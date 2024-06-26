f3aaa905e29d7adf86d221ba6c554ff745c7b80a
Quentin Schuster <quentin-schuster@tutanota.com>
2024-06-26 16:02:13 +0200

    add tooltip

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -7,10 +7,14 @@ export const Route = createFileRoute('/')({
   component: route
 })
 
-const dateFormatter = (date: any) => {
+function addLeadingZero(number: number): string {
+  return `${number < 10 ? '0' : ''}${number}`
+}
+
+function dateFormatter(date: any) {
   if (!(date instanceof Date)) return date
 
-  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  return `${addLeadingZero(date.getHours())}:${addLeadingZero(date.getMinutes())}:${addLeadingZero(date.getSeconds())}`
 };
 
 function route() {
@@ -41,19 +45,46 @@ function route() {
         </Card>
       </div>
       <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-base font-normal">Total Revenue</CardTitle>
+        <CardHeader className='pb-0'>
         </CardHeader>
         <CardContent>
           <div className='h-[25rem] lg:h-[30rem] xl:h-[30rem] 2xl:h-[48rem]'>
-            <p>{Math.round(Math.pow(window.innerWidth, 2)/ 200_000)}</p>
             <ResponsiveContainer  width="100%" height="100%">
               <LineChart
                 data={history}
                 height={800}
                 width={800}
               >
-                <Tooltip />
+                <Tooltip 
+                  content={({ active, payload}) => {
+                    if (active && payload && payload.length) {
+                      return (
+                        <div className="rounded-lg border bg-background p-2 shadow-sm">
+                          <div className="grid grid-cols-1 gap-2">
+                            <div className="flex flex-col">
+                              <span className="text-[0.70rem] uppercase text-muted-foreground">
+                                Zeit
+                              </span>
+                              <span className="font-bold text-muted-foreground">
+                                {dateFormatter(payload[0].payload.date)}
+                              </span>
+                            </div>
+                            <div className="flex flex-col">
+                              <span className="text-[0.70rem] uppercase text-muted-foreground">
+                                CO2
+                              </span>
+                              <span className="font-bold">
+                                {payload[0].value} ppm
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    }
+  
+                    return null
+                  }}  
+                />
                 <YAxis />
                 <XAxis 
                   dataKey="date"
